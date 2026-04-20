6a244c22af5773543329664fe8794edf9a4847ba
Jerome Quijano <jeromequijano17@gmail.com>
2026-04-21 07:41:59 +0800

    commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// database connection
+// database connections
 const db = mysql.createConnection({
   host: "mysql-37610bae-jeromequijano17-df99.l.aivencloud.com",
   user: "jerome",
